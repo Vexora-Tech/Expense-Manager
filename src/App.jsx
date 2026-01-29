fb29a861bbb42d439a1a9ffce039f135cb6fec66
vansh <vanshdhaka02@gmail.com>
2026-01-30 01:23:12 +0530

    file setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
