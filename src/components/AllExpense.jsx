ddc8e472f7105808b81276106f23690d54723d6b
vansh <vanshdhaka02@gmail.com>
2026-02-02 01:50:16 +0530

    added a total balance highligter

--- src/components/AllExpense.jsx
+++ src/components/AllExpense.jsx
@@ -25,19 +25,33 @@ function AllExpense() {
     return price;
   };
 
+  const totalIncome = calculateMoney("income");
+  const totalExpense = calculateMoney("expense");
+  const totalBalance = totalIncome - totalExpense;
+
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 mt-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-6 mt-6">
         <div className="w-[96%] lg:w-[80%] mx-auto py-4 px-2 rounded border border-zinc-300">
           <p className="font-bold text-2xl text-green-500">Income</p>
           <p className="text-xl font-semibold text-end text-green-400">
-            ₹ {calculateMoney("income")}
+            ₹ {totalIncome}
           </p>
         </div>
         <div className="w-[96%] lg:w-[80%] mx-auto py-4 px-2 rounded border border-zinc-300">
           <p className="font-bold text-2xl text-red-500">Expense</p>
           <p className="text-xl font-semibold text-end text-red-400">
-            ₹ {calculateMoney("expense")}
+            ₹ {totalExpense}
+          </p>
+        </div>
+        <div className="col-span-2 py-4 px-2 rounded border border-zinc-300">
+          <p className="font-bold text-2xl">Total Balance</p>
+          <p
+            className={`text-xl font-semibold text-end ${
+              totalBalance >= 0 ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            ₹ {totalBalance}
           </p>
         </div>
       </div>
